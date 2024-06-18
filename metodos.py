74cce468ad76fea47263d1f742cf063653c7d80b
icarin <icaro7988@gmail.com>
2024-06-18 19:38:02 -0300

    adicionando metodos medico

--- metodos.py
+++ metodos.py
@@ -114,3 +114,74 @@ def imprime_menu_procedimentos():
         ║  2. Listar procedimentos              ║
         ║  3. Voltar ao menu principal          ║
         ╚═══════════════════════════════════════╝ """)
+    
+
+#===============================================
+
+
+def criar_medico_principal():
+    def cria_medico():
+                    
+        testa = True
+
+        while testa: 
+            nome = input("\nInforme o nome do médico: ")
+            especialidade = input("Informe a especialidade do médico: ")
+            telefone = input("Informe o telefone do médico: ")
+
+            if not nome or not especialidade or not telefone:
+                print("\nInforme todos os dados solicitados!") 
+            else:
+                medico = { "nome": nome, "especialidade": especialidade, "telefone": telefone}
+                testa = False
+                return medico
+
+    medico = cria_medico()
+
+    nome = medico["nome"]
+    especialidade = medico["especialidade"]
+    telefone = medico["telefone"]
+
+    #insere o medico na tabela
+    sql_inserir_medico = "INSERT INTO medico (nome, especialidade, telefone) VALUES (%s, %s, %s)"
+    dados_insert = (nome, especialidade, telefone)
+    insertNaTabela(conexao, sql_inserir_medico, dados_insert)
+    print("Médico cadastrado com sucesso!")
+    print(listarTabelas(conexao, sql_listar_medicos))
+def busca_medico_pelo_crm():
+
+    crm = input("Informe o crm do medico que você deseja buscar: ")
+    cursor = conexao.cursor()
+    sql_buscar_medico = "SELECT * FROM medico WHERE crm = %s"
+    cursor.execute(sql_buscar_medico, (crm,))
+    medico = cursor.fetchone()
+    
+    if medico:
+        print(f"\nMédico encontrado:\n"
+          f"Nome: {medico[1]}\n"
+          f"especialidade: {medico[2]}\n"
+          f"telefone: {medico[3]}\n")
+    else:
+        print("Nenhum médico encontrado com o CRM informado.")
+def exclui_medico_pelo_crm():
+    #mostra os medicos cadastrados antes de excluir
+    print(listarTabelas(conexao, sql_listar_medicos))
+        
+    #remove um medico pelo crm 
+    crm = input("Informe o crm do medico que você deseja remover: ")
+    cursor = conexao.cursor()
+
+    # SQL para buscar o médico pelo CRM antes de excluir
+    sql_buscar_medico = "SELECT * FROM medico WHERE crm = %s"
+    cursor.execute(sql_buscar_medico, (crm,))
+    medico = cursor.fetchone()
+
+    # Verifica se o médico com o CRM existe
+    if not medico:
+        print(f"Médico com CRM {crm} não encontrado.")
+    else:
+        sql_remover = "DELETE FROM medico WHERE crm = %s"
+        dados_remover = (crm,)
+        excluirDadosTabela(conexao, sql_remover, dados_remover)
+        print("Médico removido com sucesso!")
+    #---------------------------------------------------
